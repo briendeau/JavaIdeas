be3437a9fa84f4db5715eda71d1718af0be9c38c
briendeau <yamori425@gmail.com>
2022-11-24 03:35:08 -0500

    Hello

---
+++ src/main/java/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello Brian.");
+        Person person = new Person("Brian");
+        System.out.println(person.getName());
+
+    }
+}
